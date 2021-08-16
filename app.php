8c699d6c9e16aa5faf7006ca01ff3ca40ba7be0f
Bakalavr Dankovskiy <bakalavr.dankovskiy@gmail.com>
2021-08-16 22:27:08 +0300

    Added SayStringComand, dev2 created

--- app.php
+++ app.php
@@ -3,6 +3,8 @@
 require_once 'vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
+use Commands\SayStringCommand;
 
 $application = new Application();
+$application->add(new SayStringCommand());
 $application->run();
