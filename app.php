08e7b5b0addbd3f016baa7b55bfd4e0f07f95163
Bakalavr Dankovskiy <bakalavr.dankovskiy@gmail.com>
2021-08-16 22:33:49 +0300

    Added AppQuestCommand, dev 3 created

--- app.php
+++ app.php
@@ -3,6 +3,8 @@
 require_once 'vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
+use Commands\AppQuestCommand;
 
 $application = new Application();
+$application->add(new AppQuestCommand());
 $application->run();
