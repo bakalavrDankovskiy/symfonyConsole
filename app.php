107c7668cbd6c30486dccc955c37f47a709a7f32
Bakalavr Dankovskiy <bakalavr.dankovskiy@gmail.com>
2021-08-16 22:24:21 +0300

    Added HelloComand, dev1 created

--- app.php
+++ app.php
@@ -3,6 +3,8 @@
 require_once 'vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
+use Commands\HelloCommand;
 
 $application = new Application();
+$application->add(new HelloCommand());
 $application->run();
---
+++ src/Commands/HelloCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HelloCommand extends Command
+{
+    protected static $defaultName = 'say_hello ';
+
+    public static function output()
+    {
+        return 'im Alive';
+    }
+
+    protected function configure(): void
+    {   //Description and help
+        $this->setDescription('Says hello');
+
+        //Required input parameter
+        $this->addArgument
+        (
+            'objectToGreet',
+            InputArgument::IS_ARRAY,
+            'Object to say hello'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        try {
+            $objectToGreet = $this->strFormatter(implode($input->getArgument('objectToGreet')));
+            if (empty($objectToGreet)) {
+                throw new InvalidArgumentException();
+            }
+            $output->writeln('Привет ' . $objectToGreet);
+        } catch (InvalidArgumentException $e) {
+            $output->writeln('Вы ввели пустую строку, попробуйте еще раз',);
+        }
+        return self::SUCCESS;
+    }
+
+    protected function strFormatter(string|null $string): string|null
+    {
+        return is_null($string) ? null : trim(trim($string, '\''));
+    }
+}
+
